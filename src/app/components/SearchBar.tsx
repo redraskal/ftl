6b87a72bd0d9a5e32682c228a59b02c267fd1e4e
Benjamin <ben@ctos.sh>
2025-04-24 16:20:15 -0500

    feat: search bar icon

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface SearchBarProps extends React.InputHTMLAttributes<HTMLInputElement> {
   query: string;
   setQuery: (query: string) => void;
@@ -5,13 +7,20 @@ interface SearchBarProps extends React.InputHTMLAttributes<HTMLInputElement> {
 
 export function SearchBar({ query, setQuery, ...delegated }: SearchBarProps) {
   return (
-    <input
-      className="w-full rounded-full bg-[#1E97C3] px-6 py-4 text-xl font-bold text-black placeholder-black shadow-[0px_4px_4px_rgba(0,0,0,0.25)]"
-      type="text"
-      placeholder="Search..."
-      value={query}
-      onChange={(e) => setQuery(e.target.value)}
-      {...delegated}
-    />
+    <div className="relative w-full">
+      <input
+        className="w-full rounded-full bg-[#1E97C3] px-6 py-4 pr-14 text-xl font-bold text-black placeholder-black shadow-[0px_4px_4px_rgba(0,0,0,0.25)]"
+        type="text"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        {...delegated}
+      />
+      <Image
+        src="/search.svg"
+        alt="Search"
+        className="absolute top-1/2 right-4 h-6 w-6 -translate-y-1/2 transform"
+      />
+    </div>
   );
 }
